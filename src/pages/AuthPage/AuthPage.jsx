dae517882a4e2e22245b45cacab91affb86509c8
jkawashiri <jkawashiri5@gmail.com>
2023-10-03 14:09:36 -0700

    displays login or signup form depending on state of the clicked button

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,12 +1,22 @@
 import SignUpForm from "../../components/SignUpForm/SignUpForm"
 import LoginForm from "../../components/LoginForm/LoginForm"
+import { useState } from "react"
 
 export default function AuthPage({setUser}) {
+    const [clicked, setClicked] = useState(true)
+    let buttonText = clicked === true ? 'Sign Up' : 'Login'
+    function onClick() {
+        setClicked(clicked => !clicked )
+    }
     return (
         <main>
             <h1>AuthPage</h1>
-            <SignUpForm setUser={setUser} />
-            <LoginForm setUser={setUser} />
+            <button onClick={onClick}>{buttonText}</button>
+            { clicked === true ? 
+                <LoginForm setUser={setUser} />
+                :
+                <SignUpForm setUser={setUser} />
+            }
         </main>
     )
 }
